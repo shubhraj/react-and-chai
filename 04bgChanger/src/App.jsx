c5fff8e0915a2d0d4117303f7ee9d33c1636eb82
Shubham patil <shubham.patil@sincrodigital.com>
2023-08-30 15:07:50 +0530

    adding the onclick to change state using useState hook, which will change the bgColor of the div

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-screen h-screen duration-200'
+      style={{backgroundColor: color}}>
+        <div className='fixed flex flex-wrap justify-center inset-x-0 px-2 py-2 bottom-0 w-full'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg'>
+              <button className='font-bold py-2 px-4 rounded text-white' style={{backgroundColor: "red"}} onClick={() => setColor('red')}> Red </button>
+              <button className='font-bold py-2 px-4 rounded text-white' style={{backgroundColor: "green"}} onClick={() => setColor('green')}> Green </button>
+              <button className='font-bold py-2 px-4 rounded text-white' style={{backgroundColor: "blue"}} onClick={() => setColor('blue')}> Blue </button>
+          </div>  
+      </div>  
+      
+    </div>
   )
 }
 
